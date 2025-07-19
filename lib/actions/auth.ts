ee31293598cee15b03902edd0f392bd1f627f81a
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-07-19 12:20:34 +0530

    feat: implement Redis integration and rate limiting; update package.json and config

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -3,19 +3,27 @@
 import { signIn } from "@/auth";
 import { db } from "@/database/drizzle";
 import { user } from "@/database/schema";
+import ratelimit from "@/ratelimit";
 import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
 ) => {
   const { email, password } = params;
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  if (!success) {
+    return redirect("/too-fast");
+  }
   try {
     const result = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-    })
+      email,
+      password,
+      redirect: false,
+    });
     if (result?.error) {
       return { success: false, error: result.error };
     }
@@ -25,11 +33,18 @@ export const signInWithCredentials = async (
     console.log(error, "signIn Error");
     return { success: false, error: "Failed to sign in" };
   }
-
 };
 
 export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, universityId, universityCard, password } = params;
+
+  // step 0 : rate limit setup using upstash
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+  if (!success) {
+    return redirect("/too-fast");
+  }
+
   // step 1: Check if the user already exists
   const existingUser = await db
     .select()
